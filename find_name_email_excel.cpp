11c6ba3de84c9bb148e919b41c0bf60f61d99558
Amit Kumar <56188935+drviruses@users.noreply.github.com>
2020-10-25 20:28:41 +0530

    Update find_name_email_excel.cpp

--- find_name_email_excel.cpp
+++ find_name_email_excel.cpp
@@ -12,6 +12,36 @@ using namespace std;
 #define ld double
 #define endl "\n"
 
+vector<string> vec_splitter(string s) {
+        s += ',';
+        vector<string> res;
+        while(!s.empty()) {
+                res.push_back(s.substr(0, s.find(',')));
+                s = s.substr(s.find(',') + 1);
+        }
+        return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+        if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+        stringstream ss; ss << H;
+        cerr << args[idx] << " = " << ss.str();
+        debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#define XOX
+#ifdef XOX
+    #define deb(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+    #define deb(...) 42
+#endif
+
+
+
 const ll mod = 1e9+7;
 const ll inf = 1e18;
 
